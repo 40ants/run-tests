a44dfe01e95fae80fd3281aaca55060b75109d7c
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-09-09 16:41:40 +0300

    Updated changelog.

--- changelog.lisp
+++ changelog.lisp
@@ -13,7 +13,7 @@
           This fixes test running when Roswell and Qlot are restored from cache.")
   (2.1.1 2021-02-19
          "Fixed test running on CLISP where cl-coveralls installation
-          fails with error: \"CFFI requires CLISP compiled with dynamic FFI support.\"")
+          fails with error: `CFFI requires CLISP compiled with dynamic FFI support`.")
   (2.1.0 2021-02-18
          "Now it is possible to conditionally select if coverage
           report should be generated.")
